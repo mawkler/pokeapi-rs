752e816b2636f8eca0b9e8060617b8e3f0401448
Melker Ulander <melker.ulander@pm.me>
2024-08-23 15:50:44 +0200

    Switch from argument to subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,44 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use reqwest::Error;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
-struct Pokemon {
-    name: String,
-    height: u32,
-    weight: u32,
+pub struct Pokemon {
+    pub name: String,
+    pub height: u32,
+    pub weight: u32,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    /// Get a pokemon
+    Get { name: String },
 }
 
 #[derive(Parser, Debug)]
 #[command(version, about = "Fetches Pokémon from PokéAPI.")]
 struct Args {
-    /// The name of the Pokémon to fetch data for
-    #[arg(short, long)]
-    pokemon: String,
+    #[command(subcommand)]
+    command: Command,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args = Args::parse();
 
-    let url = format!("https://pokeapi.co/api/v2/pokemon/{}", args.pokemon);
-    let response = reqwest::get(&url).await?;
+    match args.command {
+        Command::Get { name } => {
+            let url = format!("https://pokeapi.co/api/v2/pokemon/{}", name);
+            let response = reqwest::get(&url).await?;
 
-    if response.status().is_success() {
-        let pokemon: Pokemon = response.json().await?;
-        dbg!(&pokemon);
-    } else {
-        eprintln!("Error: Could not fetch data for Pokémon '{}'", args.pokemon);
-    }
+            if response.status().is_success() {
+                let pokemon: Pokemon = response.json().await?;
+                dbg!(&pokemon);
+            } else {
+                eprintln!("Error: Could not fetch data for Pokémon '{}'", name);
+            }
+        }
+    };
 
     Ok(())
 }
