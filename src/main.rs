a0a636965d6b0442e062747ca0ccce1fc755d571
Melker Ulander <melker.ulander@pm.me>
2024-09-03 13:26:26 +0200

    Refactor out get_pokemon()
    Also add GetPokemonError using thiserror and anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use anyhow::Context;
 use clap::{Parser, Subcommand};
-use reqwest::Error;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -9,34 +9,50 @@ pub struct Pokemon {
     pub weight: u32,
 }
 
-#[derive(Subcommand, Debug)]
+#[derive(Subcommand)]
 pub enum Command {
     /// Get a pokemon
     Get { name: String },
 }
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(version, about = "Fetches Pokémon from PokéAPI.")]
 struct Args {
     #[command(subcommand)]
     command: Command,
 }
 
+#[derive(thiserror::Error, Debug)]
+enum GetPokemonError {
+    #[error("Pokemon not found")]
+    NotFound,
+    #[error("Failed to get pokemon: {0}")]
+    Other(#[from] anyhow::Error),
+}
+
+async fn get_pokemon(name: String) -> Result<Pokemon, GetPokemonError> {
+    let url = format!("https://pokeapi.co/api/v2/pokemon/{}", name);
+    let response = reqwest::get(&url).await.context("GET request failed")?;
+
+    if response.status().is_success() {
+        let pokemon = response
+            .json()
+            .await
+            .context("failed to deserialize pokemon")?;
+        Ok(pokemon)
+    } else {
+        Err(GetPokemonError::NotFound)
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     match args.command {
         Command::Get { name } => {
-            let url = format!("https://pokeapi.co/api/v2/pokemon/{}", name);
-            let response = reqwest::get(&url).await?;
-
-            if response.status().is_success() {
-                let pokemon: Pokemon = response.json().await?;
-                dbg!(&pokemon);
-            } else {
-                eprintln!("Error: Could not fetch data for Pokémon '{}'", name);
-            }
+            let pokemon = get_pokemon(name).await?;
+            dbg!(&pokemon);
         }
     };
 
